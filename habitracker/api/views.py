d791eb2b8e5a80cbe79d3f0a9ae78a1881116acf
P V <hadi.rafad.p.v@accenture.com>
2025-04-24 00:03:51 +0530

    temperory change for creating superuser

--- habitracker/api/views.py
+++ habitracker/api/views.py
@@ -3,6 +3,16 @@ from rest_framework import viewsets
 from .models import Habit
 from .serializers import HabitSerializer
 
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+
+def create_superuser(request):
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@example.com", "admin")
+        return HttpResponse("Superuser created!")
+    return HttpResponse("Superuser already exists.")
+
 # Create your views here.
 class HabitViewSet(viewsets.ModelViewSet):
     queryset    =   Habit.objects.all()
